c70207fa633e519692c71d2f727cb7ef11672e51
Brad Breiten <jonathan@breiten.us>
2022-01-15 16:30:40 -0700

    Add and test new validations for bulk discount

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -12,5 +12,8 @@ RSpec.describe BulkDiscount, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:percent_discount) }
     it { should validate_presence_of(:threshold) }
+
+    it { should validate_numericality_of(:percent_discount).is_greater_than(0).is_less_than_or_equal_to(100) }
+    it { should validate_numericality_of(:threshold) }
   end
 end
