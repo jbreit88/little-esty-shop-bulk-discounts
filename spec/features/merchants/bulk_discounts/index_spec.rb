6ac472bed74930e1a9625189cb7d000477ce2bf3
Brad Breiten <jonathan@breiten.us>
2022-01-15 14:43:58 -0700

    Test: update expected url paths and add sad path expectation to tests

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -3,27 +3,29 @@ require 'rails_helper'
 RSpec.describe 'Merchant Bulk Discounts Index Page', type: :feature do
 
   let!(:merchant_1) {Merchant.create!(name: 'Merchant 1', status: 0)}
+  let!(:merchant_2) {Merchant.create!(name: 'Merchant 2', status: 0)}
 
   let!(:fifteen_off_ten) {merchant_1.bulk_discounts.create!(name: "Fifteen Off Ten", threshold: 10, percent_discount: 15)}
   let!(:twenty_off_fifteen) {merchant_1.bulk_discounts.create!(name: "Twenty Off Fifteen", threshold: 15, percent_discount: 20)}
+  let!(:five_off_ten) {merchant_2.bulk_discounts.create!(name: "Five Off Ten", threshold: 10, percent_discount: 5)}
 
   context 'As a merchant when I visit my dashboard' do
     scenario 'I see a link to view my discounts' do
-      visit merchant_dashboard_path(merchant_1.id)
+      visit merchant_dashboard_index_path(merchant_1.id)
 
-      expect(page).to have_link("Discounts", href: merchant_discounts_path(merchant_1.id))
+      expect(page).to have_link("Discounts", href: merchant_bulk_discounts_path(merchant_1.id))
     end
 
     scenario 'I click the link and am taken to my bulk discounts index page' do
-      visit merchant_dashboard_path(merchant_1.id)
+      visit merchant_dashboard_index_path(merchant_1.id)
 
       click_link('Discounts')
 
-      expect(current_path).to eq(merchant_discounts_path(merchant_1.id))
+      expect(current_path).to eq(merchant_bulk_discounts_path(merchant_1.id))
     end
 
     scenario 'I see all my bulk discounts and their attributes' do
-      visit merchant_discounts_path(merchant_1.id)
+      visit merchant_bulk_discounts_path(merchant_1.id)
 
       within "#bulk-discount-#{fifteen_off_ten.id}" do
         expect(page).to have_content(fifteen_off_ten.name)
@@ -36,27 +38,20 @@ RSpec.describe 'Merchant Bulk Discounts Index Page', type: :feature do
         expect(page).to have_content(twenty_off_fifteen.percent_discount)
         expect(page).to have_content(twenty_off_fifteen.threshold)
       end
+
+      expect(page).to have_no_content(five_off_ten.name)
     end
 
     scenario 'I see each bulk discount is a link to its own show page' do
+      visit merchant_bulk_discounts_path(merchant_1.id)
+
       within "#bulk-discount-#{fifteen_off_ten.id}" do
-        expect(page).to have_link(fifteen_off_ten.name, href: merchant_descount_path(merchant_1.id, fifteen_off_ten.id))
+        expect(page).to have_link(fifteen_off_ten.name, href: merchant_bulk_discounts_path(merchant_1.id, fifteen_off_ten.id))
       end
 
       within "#bulk-discount-#{twenty_off_fifteen.id}" do
-        expect(page).to have_link(twenty_off_fifteen.name, href: merchant_descount_path(merchant_1.id, twenty_off_fifteen.id))
+        expect(page).to have_link(twenty_off_fifteen.name, href: merchant_bulk_discounts_path(merchant_1.id, twenty_off_fifteen.id))
       end
     end
   end
 end
-
-# Merchant Bulk Discounts Index
-#
-# As a merchant
-# When I visit my merchant dashboard
-# Then I see a link to view all my discounts
-# When I click this link
-# Then I am taken to my bulk discounts index page
-# Where I see all of my bulk discounts including their
-# percentage discount and quantity thresholds
-# And each bulk discount listed includes a link to its show page
