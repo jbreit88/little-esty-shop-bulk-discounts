eaaf0636667b1dfcbd4428e318a3abdd6f967d50
Brad Breiten <jonathan@breiten.us>
2022-01-15 17:14:18 -0700

    Feature: Add delete button next to discount on index page, when clicked it deletes discount

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -19,6 +19,12 @@ class BulkDiscountsController < ApplicationController
     redirect_to merchant_bulk_discounts_path(merchant.id)
   end
 
+  def destroy
+    bulk_discount = BulkDiscount.destroy(params[:id])
+
+    redirect_to merchant_bulk_discounts_path(params[:merchant_id])
+  end
+
   private
 
   def bulk_discount_params
