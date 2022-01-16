36b6c9014047439cd46c25e38cfe9797bcab5d0e
Brad Breiten <jonathan@breiten.us>
2022-01-15 19:08:41 -0700

    Test: Check for existence of edit discount link on bulk discount show page

--- spec/features/merchants/bulk_discounts/show_spec.rb
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -6,15 +6,28 @@ RSpec.describe 'Bulk Discount Show Page', type: :feature do
   let!(:fifteen_off_ten) {merchant_1.bulk_discounts.create!(name: "Fifteen Off Ten", threshold: 10, percent_discount: 15)}
   let!(:twenty_off_fifteen) {merchant_1.bulk_discounts.create!(name: "Twenty Off Fifteen", threshold: 15, percent_discount: 20)}
 
+  before(:each) do
+    visit merchant_bulk_discount_path(merchant_1.id, fifteen_off_ten.id)
+  end
+
   context 'As a merchant, when I visit a bulk discount show page' do
-    scenario 'I see discount quantity threshold and percentage discount' do
-      visit merchant_bulk_discount_path(merchant_1.id, fifteen_off_ten.id)
+    scenario 'I see discount quantity threshold and percentage discount attributes' do
 
       expect(page).to have_content(fifteen_off_ten.name)
       expect(page).to have_content(fifteen_off_ten.percent_discount)
       expect(page).to have_content(fifteen_off_ten.threshold)
 
       expect(page).to have_no_content(twenty_off_fifteen.name)
+    end
+
+    scenario 'I see a link to edit the bulk discount' do
+      expect(page).to have_link('Edit Discount', href: edit_merchant_bulk_discount_path(merchant_1.id, fifteen_off_ten.id))
+    end
+
+    scenario 'When I click the edit link I am taken to an edit page' do
+      click_link 'Edit Discount'
+
+      expect(current_path).to eq(edit_merchant_bulk_discount_path(merchant_1.id, fifteen_off_ten.id))
     end
   end
 end
