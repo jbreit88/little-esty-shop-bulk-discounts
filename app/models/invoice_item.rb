78bcd01a12e2aa1bd25d881b452fd480117137a5
Brad Breiten <jonathan@breiten.us>
2022-01-18 09:45:51 -0700

    Add: instance method to find a bulk discount Test: cover the above addition

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -13,6 +13,12 @@ class InvoiceItem < ApplicationRecord
   has_many :bulk_discounts, through: :item
 
   def self.revenue
-      sum('quantity * unit_price')
+    sum('quantity * unit_price')
+  end
+
+  def find_bulk_discount
+    bulk_discounts.where("bulk_discounts.threshold <= ?", quantity)
+    .order(percent_discount: :desc)
+    .first
   end
 end
