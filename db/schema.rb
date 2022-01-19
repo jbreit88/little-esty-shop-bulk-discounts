47af98757f53cbe36b3c113d13189857d8bf5dec
Brad Breiten <jonathan@breiten.us>
2022-01-18 20:49:35 -0700

    Add migrations to database: add type for STI and holiday for filtering BulkDiscounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_15_210229) do
+ActiveRecord::Schema.define(version: 2022_01_18_235757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2022_01_15_210229) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "percent_discount"
+    t.string "type"
+    t.string "holiday"
     t.index ["merchant_id"], name: "index_bulk_discounts_on_merchant_id"
   end
 
